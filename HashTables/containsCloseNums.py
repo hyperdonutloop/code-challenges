4c04a60f75a440de953c33ea5b882265a06805ba
donutwizard666 <thebeardedwarrior@icloud.com>
2019-09-01 11:53:21 -0700

    added in psuedocode and steps to solve

--- HashTables/containsCloseNums.py
+++ HashTables/containsCloseNums.py
@@ -7,6 +7,16 @@ Given an array of integers nums and an integer k, determine whether there are tw
 # Start with an empty hash table hash. When iterating through the list num, store hash[num[i]] = i if num[i] isn't a key in the hash table. If num[i] is a key in the hash table, check if the current index and the last seen index (stored as the value) are within k of each other. 
 # If not, update the last seen index in hash[num[i]] to the current index i.
 
+nums = [0, 1, 2, 3, 5, 2]
+k = 3
 
 def containsCloseNums(nums, k):
-  
+  # create a hashtable
+  # iterate through num list
+  # if num[i] is not a key in the HT,
+    # store hash[num[i]] = i
+  # else if it is
+    # check if current index and last seen index are within k of eachother
+    # if they are not update the last seen index in hash[num[i]] to the current index i
+
+
