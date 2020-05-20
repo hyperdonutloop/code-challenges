ae1e7e00a8d52ea649eb9b890283143626adf63c
donutwizard666 <thebeardedwarrior@icloud.com>
2020-05-20 10:28:15 -0700

    added pseudocode

--- Intro/TheEdgeOfTheOcean.py
+++ Intro/TheEdgeOfTheOcean.py
@@ -28,3 +28,8 @@ def shapeArea(n):
 
 # Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
 
+# arrange smallest to largest
+# each statue will be bigger than previous by 1
+# what is missing from statue array?
+# [6, 2, 3, 8]
+# sort 
