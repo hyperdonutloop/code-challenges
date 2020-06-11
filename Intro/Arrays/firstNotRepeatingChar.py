117b791c24bb4dd14b5f3d0e9f768a7014d59165
donutwizard666 <thebeardedwarrior@icloud.com>
2020-06-11 11:15:38 -0700

    added solution one

--- Intro/Arrays/firstNotRepeatingChar.py
+++ Intro/Arrays/firstNotRepeatingChar.py
@@ -10,4 +10,17 @@ Given a string s consisting of small English letters, find and return the first 
 # if no character found
 # return - 
 
+# solution 1
 def firstNotRepeatingCharacter(s):
+  order = []
+  counts = {}
+  for char in s:
+    if char in counts:
+      counts[char] += 1
+    else:
+      counts[char] = 1
+      order.append(char)
+  for char in order:
+    if counts[char] == 1:
+      return char
+  return "_"
