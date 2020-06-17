b35afd5dec4859fe8681f51f34082c4d12a2e13c
donutwizard666 <thebeardedwarrior@icloud.com>
2020-06-17 11:02:22 -0700

    solution one and clues added

--- HashTables/groupingDIshes.py
+++ HashTables/groupingDIshes.py
@@ -4,5 +4,23 @@ You are given a list dishes, where each element consists of a list of strings be
 Return an array where each element is a list beginning with the ingredient name, followed by the names of all the dishes that contain this ingredient. The dishes inside each list should be sorted lexicographically, and the result array should be sorted lexicographically by the names of the ingredients.
 
 """
+# Clue 1 - Make a hash table where the key is the ingredient, and the value associated with an ingredient is an array of dishes that use that ingredient.
 
+# Clue 2 - Once you have iterated through all the dishes and each ingredient is associated with an array of dishes as a value, sort each value. THEN prepend the key (the ingredient) to the value.
+
+# Clue 3 - Get a list of values from the hash table, and sort it. It should sort by the first element (i.e. the ingredient) first.
+
+# Found solution one
 def groupingDishes(dishes):
+    hashTable = {}
+    for dish in dishes:
+        for ingredient in dish[1:]:
+            if ingredient in hashTable:
+                hashTable[ingredient].append(dish[0])
+            else:
+                hashTable[ingredient] = [dish[0]]
+    ans = []
+    for k, v in hashTable.items():
+        if len(v) > 1:
+            ans.append([k] + sorted(v))
+    return sorted(ans)
