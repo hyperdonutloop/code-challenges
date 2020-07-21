2be57acc4a91c5908568c58af03b39192623f2b4
donutwizard666 <thebeardedwarrior@icloud.com>
2020-07-20 21:13:47 -0700

    solved problem two

--- Intro/TheJourneyBegins.js
+++ Intro/TheJourneyBegins.js
@@ -3,3 +3,9 @@
 function add(param1, param2) {
   return param1 + param2
 }
+
+// Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+function centuryFromYear(year) {
+  return Math.ceil(year/100)
+}
