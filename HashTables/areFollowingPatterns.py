1a223584c90f5aace624eb6aac7aa117b60050f1
donutwizard666 <thebeardedwarrior@icloud.com>
2020-07-31 09:23:55 -0700

    added second problem

--- HashTables/areFollowingPatterns.py
+++ HashTables/areFollowingPatterns.py
@@ -3,12 +3,33 @@ Given an array strings, determine whether it follows the sequence given in the p
 """
 
 # found solution one
+# strings = ["cat", "dog", "dog"]
+# patterns = ["a", "b", "b"]
+
+# def areFollowingPatterns(strings, patterns):
+#   n = len(strings)
+#   for i in range(n):
+#     for j in range(i+1,n):
+#       if strings[i]==strings[j] and patterns[i]!=patterns[j]:
+#         return False
+#       if strings[i]!=strings[j] and patterns[i]==patterns[j]:
+#         return False
+#   return True
+
+# areFollowingPatterns(strings, patterns)
+
+# found very short solution
+strings = ["cat", "dog", "dog"]
+
+patterns = ["a", "b", "b"]
+
 def areFollowingPatterns(strings, patterns):
-  n = len(strings)
-  for i in range(n):
-    for j in range(i+1,n):
-      if strings[i]==strings[j] and patterns[i]!=patterns[j]:
-        return False
-      if strings[i]!=strings[j] and patterns[i]==patterns[j]:
-        return False
-  return True
+  print(set(strings))
+  print(set(patterns))
+  print(set(zip(strings,patterns)))
+  if len(set(strings)) == len(set(patterns)) == len(set(zip(strings, patterns))):
+    return True
+  else:
+    return False
+
+areFollowingPatterns(strings, patterns)
